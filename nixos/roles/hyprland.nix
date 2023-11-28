3f124007db9f6e5a10df2fd7bf2a8715633e0782
paveladamek <pavel.adamek241@gmail.com>
2023-11-28 15:13:49 +0100

    s

--- nixos/roles/hyprland.nix
+++ nixos/roles/hyprland.nix
@@ -13,12 +13,46 @@ in
 
   config = lib.mkIf cfg.enable
     {
-      programs.hyprland = {
+      i18n.inputMethod.enabled = "fcitx5";
+      i18n.inputMethod.fcitx5.addons = [ pkgs.fcitx5-mozc ];
+
+      security.pam.services.swaylock = { };
+
+      services.udisks2 = {
         enable = true;
+        mountOnMedia = true;
       };
 
-      programs.hyprland.xwayland = {
+      services.xserver = {
         enable = true;
+        excludePackages = [ pkgs.xterm ];
       };
+
+      services.xserver = {
+        enable = true;
+
+        desktopManager = {
+          plasma5.enable = true;
+        };
+
+        displayManager = {
+          sddm.enable = true;
+
+          autoLogin = {
+            enable = true;
+            user = "barney";
+          };
+        };
+
+        excludePackages = [ pkgs.xterm ];
+      };
+
+      systemd.services = {
+        "getty@tty1".enable = false;
+        "autovt@tty1".enable = false;
+      };
+
+      programs.hyprland.enable = true;
     };
 }
+
