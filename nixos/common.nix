bcc40dc6dc219869c2374110b02b815c702e5e94
paveladamek <pavel.adamek241@gmail.com>
2024-01-17 11:21:55 +0100

    fonts

--- nixos/common.nix
+++ nixos/common.nix
@@ -86,6 +86,29 @@
     DefaultTimeoutStopSec=10s
   '';
 
+  fonts = {
+    #This is depricated new sytax will
+    packages = with pkgs; [
+      #be enforced in the next realease
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      font-awesome
+      source-han-sans
+      source-han-sans-japanese
+      source-han-serif-japanese
+      (nerdfonts.override { fonts = [ "Meslo" ]; })
+    ];
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        monospace = [ "Meslo LG M Regular Nerd Font Complete Mono" ];
+        serif = [ "Noto Serif" "Source Han Serif" ];
+        sansSerif = [ "Noto Sans" "Source Han Sans" ];
+      };
+    };
+  };
+
   # age.secrets = {
   #   influxdb-telegraf.file = ./secrets/influxdb-telegraf.age;
   #   tailscale.file = ./secrets/tailscale.age;
