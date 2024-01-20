ab3b040e245c4cce5e1a4553b741aaefc4a93ede
paveladamek <pavel.adamek241@gmail.com>
2024-01-20 14:30:29 +0100

    s

--- nixos/programs/zsh.nix
+++ nixos/programs/zsh.nix
@@ -66,7 +66,7 @@
           owner = "spaceship-prompt";
           repo = "spaceship-prompt";
           rev = "v4.15.0";
-          sha256 = "QCaZCSgg5BF8d2IIatkDBbVBL5CpzwoF94qfu2e4zTo";
+          sha256 = "0cxikpiq7hw210cz25fgn70d7kldk5prwcr29b33zgc72fdnad3l";
         };
         file = "spaceship.zsh-theme";
       }
