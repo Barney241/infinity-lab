96f6f1a1aab98e26b65569555385378f9c549b45
Pavel Adamek <pavel.adamek241@gmail.com>
2025-05-26 09:37:53 +0200

    update dev tools

--- nixos/components/dev.nix
+++ nixos/components/dev.nix
@@ -1,60 +1,54 @@
 { config, pkgs, lib, ... }:
-let
-  cfg = config.roles.dev;
-in
-{
-  options.roles.dev = {
-    enable = lib.mkEnableOption "Enable dev tools";
-  };
+let cfg = config.roles.dev;
+in {
+  options.roles.dev = { enable = lib.mkEnableOption "Enable dev tools"; };
 
-  config = lib.mkIf cfg.enable
-    {
-      environment.systemPackages = with pkgs;[
-        bash
-        doctl
-        postgresql_16
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      bash
+      doctl
+      postgresql_16
 
-        kubectl
-        kdash # kubernetes dashboard
+      kubectl
+      kdash # kubernetes dashboard
 
-        nil
-        rustup
-        # go
-        go_1_23
-        natscli
+      nil
+      rustup
+      # go
+      go_1_23
+      natscli
 
-        #rust
-        rustc
-        cargo
-        pkg-config
-        openssl
-        rustfmt
-        rustup
-        
-        #lsp
-        buf
+      #rust
+      rustc
+      cargo
+      pkg-config
+      openssl
+      rustfmt
+      rustup
 
-        nodejs_20
-        cachix
-        gnumake
-        docker-compose
-        # jetbrains.jdk
-        # jetbrains.goland
-        jetbrains.idea-ultimate
-        code-cursor
-        # jetbrains.pycharm-professional
-        # jetbrains.phpstorm
-        # jetbrains.webstorm
-        # poetry
-      ];
+      #lsp
+      buf
 
+      nodejs_20
+      cachix
+      gnumake
+      docker-compose
+      # jetbrains.jdk
+      # jetbrains.goland
+      jetbrains.idea-ultimate
+      code-cursor
+      # jetbrains.pycharm-professional
+      # jetbrains.phpstorm
+      # jetbrains.webstorm
+      # poetry
+    ];
 
-      programs.direnv = {
-        enable = true;
-        loadInNixShell = true;
-        nix-direnv = {
-          enable = true;
-        };
-      };
+    services.vscode-server.enable = true;
+
+    programs.direnv = {
+      enable = true;
+      loadInNixShell = true;
+      nix-direnv = { enable = true; };
     };
+  };
 }
